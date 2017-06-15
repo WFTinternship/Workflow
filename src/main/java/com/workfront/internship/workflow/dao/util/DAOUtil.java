d0e5e18f82e73215e8f47a8e39a1986d49ca9e1e
BAZIYAN95 <BAZIYAN95@MAIL.RU>
2017-06-16 00:36:05 +0400

    CommentDAOSpringImpl

--- src/main/java/com/workfront/internship/workflow/dao/util/DAOUtil.java
+++ src/main/java/com/workfront/internship/workflow/dao/util/DAOUtil.java
@@ -1,5 +1,7 @@
 package com.workfront.internship.workflow.dao.util;
 
+import com.workfront.internship.workflow.domain.Comment;
+import com.workfront.internship.workflow.domain.Post;
 import com.workfront.internship.workflow.domain.User;
 import org.apache.log4j.Logger;
 
@@ -13,7 +15,9 @@ public class DAOUtil {
 
     private static final Logger LOGGER = Logger.getLogger(DAOUtil.class);
 
-    public static final String id = "id";
+
+    // USER FIELDS
+    public static final String user_id = "id";
     public static final String firstName = "first_name";
     public static final String lastName = "last_name";
     public static final String email = "email";
@@ -21,13 +25,49 @@ public class DAOUtil {
     public static final String avatarURl = "avatar_url";
     public static final String rating = "rating";
 
+    // COMMENT FIELDS
+    public static final String comment_id = "id";
+    public static final String comment_user_id = "user_id";
+    public static final String comment_post_id = "post_id";
+    public static final String comment_content = "content";
+    public static final String comment_time = "comment_time";
+
+    // POST FIELDS
+    public static final String post_id = "id";
+    /**
+     * Sets comemnts values from result set
+     */
+    public static Comment commentFromResultSet(ResultSet rs){
+
+        User user = new User();
+        Post post = new Post();
+        Comment comment = new Comment();
+        try{
+            user = userFromResultSet(rs);
+            user.setId(rs.getLong(user_id));
+
+            post = postFromResultSet(rs);
+            post.setId(rs.getLong(post_id));
+
+            comment.setId(rs.getLong(comment_id));
+            comment.setUser(user);
+            comment.setPost(post);
+            comment.setContent(rs.getString(comment_content));
+            comment.setCommentTime(rs.getTimestamp(comment_time));
+
+        }catch (SQLException e) {
+            LOGGER.error("SQL exception");
+            throw new RuntimeException(e);
+        }
+        return comment;
+    }
     /**
      * Sets users fields values from result set
      */
     public static User userFromResultSet(ResultSet rs) {
         User user = new User();
         try {
-            user.setId(rs.getLong(id));
+            user.setId(rs.getLong(user_id));
             user.setFirstName(rs.getString(firstName));
             user.setLastName(rs.getString(lastName));
             user.setEmail(rs.getString(email));
@@ -40,5 +80,13 @@ public class DAOUtil {
             throw new RuntimeException(e);
         }
         return user;
+    }
+
+    /**
+     * Sets posts fields values from result set
+     */
+    public static Post postFromResultSet(ResultSet rs){
+        Post post = new Post();
+        return post;
     }
 }
