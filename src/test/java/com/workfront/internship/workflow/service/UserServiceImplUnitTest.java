6e066d1d961a42f855aff81391e12a80ad632001
Vahagn <vahagn.vardanyan@gmail.com>
2017-06-22 18:05:56 +0500

    Unit tests added

--- src/test/java/com/workfront/internship/workflow/service/UserServiceImplUnitTest.java
+++ src/test/java/com/workfront/internship/workflow/service/UserServiceImplUnitTest.java
@@ -1,6 +1,7 @@
 package com.workfront.internship.workflow.service;
 
 import com.workfront.internship.workflow.dao.springJDBC.UserDAOSpringImpl;
+import com.workfront.internship.workflow.domain.AppArea;
 import com.workfront.internship.workflow.domain.User;
 import com.workfront.internship.workflow.exceptions.service.DuplicateEntryException;
 import com.workfront.internship.workflow.exceptions.service.InvalidObjectException;
@@ -21,8 +22,7 @@ import static junit.framework.Assert.assertTrue;
 import static junit.framework.Assert.fail;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Vahag on 6/22/2017
@@ -179,7 +179,6 @@ public class UserServiceImplUnitTest extends BaseUnitTest {
         } catch (RuntimeException e) {
             assertTrue(e instanceof InvalidObjectException);
         }
-
     }
 
     @Test(expected = ServiceLayerException.class)
@@ -200,5 +199,167 @@ public class UserServiceImplUnitTest extends BaseUnitTest {
         assertEquals(actualUser, user);
     }
 
+    @Test(expected = InvalidObjectException.class)
+    public void getAppAreasById_negativeId() {
+        userService.getAppAreasById(-1);
+    }
 
+    @Test(expected = ServiceLayerException.class)
+    public void getAppAreasById_DAOException() {
+        doThrow(RuntimeException.class).when(userDAOMock).getAppAreasById(anyInt());
+
+        //Test method
+        userService.getAppAreasById(123);
+    }
+
+    @Test
+    public void getAppAreasById_success() {
+        List<AppArea> appAreas = new ArrayList<>();
+        doReturn(appAreas).when(userDAOMock).getAppAreasById(anyInt());
+
+        //Test method
+        List<AppArea> actualAppAreas = userService.getAppAreasById(123);
+        assertEquals(appAreas, actualAppAreas);
+    }
+
+    @Test
+    public void subscribeToArea_negativeId() {
+        try {
+            //Test method
+            userService.subscribeToArea(-1, 5);
+            fail();
+        } catch (RuntimeException e) {
+            assertTrue(e instanceof InvalidObjectException);
+        }
+
+        try {
+            //Test method
+            userService.subscribeToArea(5, -1);
+            fail();
+        } catch (RuntimeException e) {
+            assertTrue(e instanceof InvalidObjectException);
+        }
+    }
+
+    @Test(expected = ServiceLayerException.class)
+    public void subscribeToArea_DAOException() {
+        doThrow(RuntimeException.class).when(userDAOMock).subscribeToArea(anyInt(), anyInt());
+
+        //Test method
+        userService.subscribeToArea(10, 1);
+    }
+
+    @Test
+    public void subscribeToArea_success() {
+        //Test method
+        userService.subscribeToArea(1, 1);
+        verify(userDAOMock, times(1)).subscribeToArea(anyInt(), anyInt());
+    }
+
+    @Test
+    public void unsubscribeToArea_negativeId() {
+        try {
+            //Test method
+            userService.unsubscribeToArea(-1, 5);
+            fail();
+        } catch (RuntimeException e) {
+            assertTrue(e instanceof InvalidObjectException);
+        }
+
+        try {
+            //Test method
+            userService.unsubscribeToArea(5, -1);
+            fail();
+        } catch (RuntimeException e) {
+            assertTrue(e instanceof InvalidObjectException);
+        }
+    }
+
+    @Test(expected = ServiceLayerException.class)
+    public void unsubscribeToArea_DAOException() {
+        doThrow(RuntimeException.class).when(userDAOMock).unsubscribeToArea(anyInt(), anyInt());
+
+        //Test method
+        userService.unsubscribeToArea(10, 1);
+    }
+
+    @Test
+    public void unsubscribeToArea_success() {
+        //Test method
+        userService.unsubscribeToArea(1, 1);
+        verify(userDAOMock, times(1)).unsubscribeToArea(anyInt(), anyInt());
+    }
+
+    @Test(expected = InvalidObjectException.class)
+    public void deleteById_negativeId() {
+        //Test method
+        userService.deleteById(-1);
+    }
+
+    @Test(expected = ServiceLayerException.class)
+    public void deleteById_DAOException() {
+        doThrow(RuntimeException.class).when(userDAOMock).deleteById(anyInt());
+
+        //Test method
+        userService.deleteById(1);
+    }
+
+    @Test
+    public void deleteById_success() {
+        //Test method
+        userService.deleteById(1);
+        verify(userDAOMock, times(1)).deleteById(anyInt());
+    }
+
+    @Test(expected = ServiceLayerException.class)
+    public void deleteAll_DAOException() {
+        doThrow(RuntimeException.class).when(userDAOMock).deleteAll();
+
+        //Test method
+        userService.deleteAll();
+    }
+
+    @Test
+    public void deleteAll_success() {
+        //Test method
+        userService.deleteAll();
+        verify(userDAOMock, times(1)).deleteAll();
+    }
+
+    @Test
+    public void authenticate_passwordIsEmpty() {
+        try {
+            //Test method
+            userService.authenticate("A", "");
+            fail();
+        } catch (RuntimeException e) {
+            assertTrue(e instanceof InvalidObjectException);
+        }
+
+        try {
+            //Test method
+            userService.authenticate("A", null);
+            fail();
+        } catch (RuntimeException e) {
+            assertTrue(e instanceof InvalidObjectException);
+        }
+    }
+
+    @Test(expected = ServiceLayerException.class)
+    public void authenticate_DAOException() {
+        doThrow(RuntimeException.class).when(userDAOMock).getById(anyInt());
+
+        //Test method
+        userService.getById(123);
+    }
+
+    @Test
+    public void authenticate_success() {
+        User user = new User();
+        doReturn(user).when(userDAOMock).getById(anyInt());
+
+        //Test method
+        User actualUser = userService.getById(123);
+        assertEquals(actualUser, user);
+    }
 }
