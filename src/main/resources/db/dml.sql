e7cfea1af51d414bbdcf600ec444b65fc4b64565
BAZIYAN95 <BAZIYAN95@MAIL.RU>
2017-06-30 15:00:08 +0400

    add new comment

--- src/main/resources/db/dml.sql
+++ src/main/resources/db/dml.sql
@@ -1,7 +1,7 @@
 /*INSERT INTO user(first_name, last_name, email, passcode, rating)
-VALUES ( 'John', 'Smith', 'john@gmail.com', '123465', 0),
-  ( 'Bob', 'Dilan', 'bob@gmail.com', '654977', 0),
-  ( 'Ken', 'Black', 'ken@gmail.com', '321654', 0);*/
+VALUES ( 'John', 'Smith', 'john@gmail.com', '123465', 0);*/
+  /* ( 'Bob', 'Dilan', 'bob@gmail.com', '654977', 0),
+   ( 'Ken', 'Black', 'ken@gmail.com', '321654', 0);*/
 
 /*INSERT INTO apparea VALUES (1, "Reporting", "Some rep description", "Team1"),
 (2, "API", "API", "Team1"),
@@ -10,15 +10,15 @@ VALUES ( 'John', 'Smith', 'john@gmail.com', '123465', 0),
 (5, "Announcement Center", "Announcement Center", "Team1"),
 (6, "Application Server", "Application Server", "Team1");*/
 
-
-/*INSERT INTO post (post_id, user_id, post_time, title, content, apparea_id)
-VALUES ( NULL, 536, '11/11/11 11:00:00', 'Post12', 'Post 12 postContent',6),
- ( NULL, 536, '11/11/11 11:00:00', 'Post12', 'Post 12 postContent',6),
+/*
+INSERT INTO post (post_id, user_id, post_time, title, content, apparea_id)
+VALUES ( NULL, 1, '11/11/11 11:00:00', 'Post12', 'kjhfiehffffffffkfksfhkhkah',6);*/
+ /*( NULL, 536, '11/11/11 11:00:00', 'Post12', 'Post 12 postContent',6),
  ( NULL, 537, '11/11/11 11:00:00', 'Post12', 'Post 12 postContent',6);*/
 
-/*INSERT INTO comment(user_id, post_id, content,comment_time)
-VALUES ( 416, 275, 'Comment1', '11/11/11 11:00:00'),
-  ( 415, 276, 'Comment2', '10/10/10 11:00:00'),
-  ( 417, 275, 'Comment2', '10/10/10 11:00:00'),
-  ( 415, 275, 'Comment2', '10/10/10 11:00:00');
+INSERT INTO comment(user_id, post_id, content,comment_time)
+VALUES ( 1, 1, 'if i can add comment', '11/11/11 11:00:00');
+/*( 415, 276, 'Comment2', '10/10/10 11:00:00'),
+( 417, 275, 'Comment2', '10/10/10 11:00:00'),
+( 415, 275, 'Comment2', '10/10/10 11:00:00');
 */
